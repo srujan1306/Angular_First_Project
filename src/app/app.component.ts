9842006d15f750b4f5a4a9e9a32af2d901ac2dbf
srujan1306 <srujan.pothu@proclink.com>
2024-07-24 12:38:03 +0530

    for loop is used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 
@@ -9,4 +9,24 @@ import { UserComponent } from './user/user.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  @Input() Students = [
+    {
+      id: 1,
+      name: 'Srujan',
+      image:
+        'https://i.pinimg.com/236x/f0/a5/69/f0a5695283fa588c815fc59a03715b15.jpg',
+    },
+    {
+      id: 2,
+      name: 'Neha',
+      image: 'https://scx2.b-cdn.net/gfx/news/hires/2023/faces.jpg',
+    },
+    {
+      id: 3,
+      name: 'Phani',
+      image:
+        'https://images.nightcafe.studio//assets/man-in-suit.jpg?tr=w-1600,c-at_max',
+    },
+  ];
+}
